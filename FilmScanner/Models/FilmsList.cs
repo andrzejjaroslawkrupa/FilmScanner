5b64896c0f4dba4450606237f9c8467be17e3cd8
Andrzej <andrzej.jaroslaw.krupa@gmail.com>
2020-02-11 17:33:38 +0100

    Added entity framework nugets and first models

---
+++ FilmScanner/Models/FilmsList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmScanner.Models
+{
+	public class FilmsList
+	{
+		public int Id { get; set; }
+		public int ExternalId { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime CreatedAt { get; set; }
+	}
+}
---
+++ FilmScanner/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmScanner.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime CreatedAt { get; set; }
+	}
+}
