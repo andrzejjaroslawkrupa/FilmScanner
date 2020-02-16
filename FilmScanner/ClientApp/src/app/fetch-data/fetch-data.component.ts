d446254b6aae952c36cfcc5270b2cee5ec427538
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2020-02-16 14:26:45 +0100

    Implemented first prototype of usage for external movie list api

--- FilmScanner/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ FilmScanner/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,19 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {MoviesService} from './movie-service';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public movies = [];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+  constructor(private _moviesService: MoviesService) {
   }
-}
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+  ngOnInit() {
+    this._moviesService.getMovies()
+      .subscribe(data => this.movies = data);
+  }
 }
---
+++ FilmScanner/ClientApp/src/app/fetch-data/movie-service.ts
@@ -0,0 +1,18 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { IMovie } from './imovie';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class MoviesService {
+
+  private _url: string = environment.apiUrl + "/?s=batman&apikey=" + environment.apiKey;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getMovies(): Observable<IMovie[]> {
+    return this.http.get<IMovie[]>(this._url);
+  }
+}
