7c25e5584b60e3ef54a2226c12bbe5b6195ae558
Andrzej <andrzej.jaroslaw.krupa@gmail.com>
2020-03-04 17:56:17 +0100

    FIL-8 Implemented radio behavior for buttons

--- FilmScanner/ClientApp/src/app/home/home.component.ts
+++ FilmScanner/ClientApp/src/app/home/home.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isPremieresChosen: boolean = true;
+  isMostPopularChosen: boolean = false;
+  isComediesChosen: boolean = false;
+  isThrillersChosen: boolean = false;
+  isActionChosen: boolean = false;
+
+  resetAllBooleans(){
+    this.isPremieresChosen = false;
+    this.isMostPopularChosen = false;
+    this.isComediesChosen = false;
+    this.isThrillersChosen = false;
+    this.isActionChosen = false;
+  }
 }
