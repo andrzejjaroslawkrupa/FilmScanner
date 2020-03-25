15cc8813161e1e2be8fb0c242ab1d7e5258635b4
Krupa <andrzej.krupa@zeiss.com>
2020-03-25 19:46:24 +0100

    FIL-56 Implemented first prototype of animation

--- FilmScanner/ClientApp/src/app/home/home.component.ts
+++ FilmScanner/ClientApp/src/app/home/home.component.ts
@@ -3,11 +3,48 @@ import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../movies/movie.service';
 import { Router } from '@angular/router';
 import { SearchResult } from '../movies/SearchResult';
+import { trigger, state, style, transition, animate, AnimationEvent } from '@angular/animations';
 
 @Component({
+
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('transform', [
+      state('visible-center', style({
+        opacity: 1,
+        marginLeft: 0,
+        marginRight: 0
+      })),
+      state('vanished-left', style({
+        opacity: 0,
+        marginLeft: '20vw',
+        marginRight: '-20vw'
+      })),
+      state('vanished-left2', style({
+        opacity: 0,
+        marginLeft: '20vw',
+        marginRight: '-20vw'
+      })),
+      state('vanished-right', style({
+        opacity: 0,
+        marginLeft: '-20vw',
+        marginRight: '20vw'
+      })),
+      state('vanished-right2', style({
+        opacity: 0,
+        marginLeft: '-20vw',
+        marginRight: '20vw'
+      })),
+      transition('* <=> void', [
+        animate('0s')
+      ]),
+      transition('* <=> *', [
+        animate('0.6s')
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
   imageLoader = true;
@@ -21,6 +58,14 @@ export class HomeComponent implements OnInit {
   sliceTop = 5;
   page = 1;
   category = 'spider-man';
+  private _state = 'visible-center';
+
+  public get state() {
+    return this._state;
+  }
+  public set state(value) {
+    this._state = value;
+  }
 
   constructor(private _appTitleService: AppTitleService, private _moviesService: MoviesService, private _router: Router) {
   }
@@ -28,6 +73,22 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this._appTitleService.setTitle('Home');
     this.subscribeToSearchResult();
+  }
+
+  rotateCarousel(event: AnimationEvent) {
+    if (event.toState == 'vanished-left') {
+      this.leftArrowClick();
+      this.state = 'vanished-right2';
+    }
+
+    if (event.toState == 'vanished-right') {
+      this.rightArrowClick();
+      this.state = 'vanished-left2';
+    }
+
+    if (event.toState == 'vanished-right2' || event.toState == 'vanished-left2') {
+      this.state = 'visible-center';
+    }
   }
 
   changeCategory(category: string) {
@@ -69,7 +130,7 @@ export class HomeComponent implements OnInit {
   }
 
   rightArrowClick() {
-    if ( this.page === 1 && this.sliceTop === 5) {
+    if (this.page === 1 && this.sliceTop === 5) {
       return;
     }
     if (this.sliceTop === 10) {
