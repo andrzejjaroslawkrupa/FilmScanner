3c3f33a8841a1f2181d9187a5df5304b711928cc
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2020-05-13 21:22:07 +0200

    FIL-63 Added first test

--- FilmScanner/ClientApp/src/app/carousel/carousel.component.spec.ts
+++ FilmScanner/ClientApp/src/app/carousel/carousel.component.spec.ts
@@ -1,25 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarouselComponent } from './carousel.component';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CarouselComponent', () => {
   let component: CarouselComponent;
   let fixture: ComponentFixture<CarouselComponent>;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CarouselComponent ]
+      imports: [BrowserAnimationsModule],
+      declarations: [CarouselComponent],
+      providers: [
+        {
+          provide: Router,
+          useClass: RouterTestingModule
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not show right-arrow on page 1', () => {
+    const rightArrowElement = element.querySelectorAll('button');
+
+    component.currentPage = 0;
+    fixture.detectChanges();
+
+    expect(rightArrowElement[0].hasAttribute('hidden')).toEqual(false);
+    expect(rightArrowElement[1].hasAttribute('hidden')).toEqual(true);
+  });
+
 });
