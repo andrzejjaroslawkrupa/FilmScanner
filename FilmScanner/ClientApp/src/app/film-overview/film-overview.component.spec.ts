5a09c19aebf3870e5ec72f799375c857aa258c83
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2020-06-18 00:02:23 +0200

    FIL-6 Removed unused service and started testing overview

--- FilmScanner/ClientApp/src/app/film-overview/film-overview.component.spec.ts
+++ FilmScanner/ClientApp/src/app/film-overview/film-overview.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilmOverviewComponent } from './film-overview.component';
+import { AppTitleService } from '../services/appTitle.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { FilmsService } from '../films/films.service';
 
 describe('FilmOverviewComponent', () => {
   let component: FilmOverviewComponent;
@@ -8,7 +12,12 @@ describe('FilmOverviewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilmOverviewComponent ]
+      declarations: [ FilmOverviewComponent ],
+      providers: [
+        {provide: AppTitleService, useValue: {getTitle: () => 'title'}},
+        {provide: ActivatedRoute, useValue: {params: of({id: 123})} },
+        {provide: FilmsService, useValue: {searchFilmByID: of()}}
+      ]
     })
     .compileComponents();
   }));
@@ -20,6 +29,6 @@ describe('FilmOverviewComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+   expect(component).toBeTruthy();
   });
 });
