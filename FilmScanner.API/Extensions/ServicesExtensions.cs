a6fc7672ef7eab2c99f84d90ddd46651d1784d89
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2022-11-20 21:24:12 +0100

    Create compose to connect db and api

--- FilmScanner.API/Extensions/ServicesExtensions.cs
+++ FilmScanner.API/Extensions/ServicesExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FilmScanner.Contracts;
+using FilmScanner.Entities;
+using FilmScanner.Repository;
+using Microsoft.EntityFrameworkCore;
 using OmdbServicesLibs.Omdb;
 using OmdbServicesLibs.Omdb.Interfaces;
 using OmdbServicesLibs.Services;
@@ -7,13 +10,33 @@ namespace FilmScanner.API.Extensions
 {
     public static class ServicesExtensions
     {
-        public static void AddOmdbServices(this IServiceCollection services, string apiKey)
+        private const string OMDB_API_KEY = "OMDB_API_KEY";
+        private const string DB_CONNECTION_STRING = "DB_CONNECTION_STRING";
+
+        public static void AddOmdbServices(this IServiceCollection services)
         {
+            var apiKey = Environment.GetEnvironmentVariable(OMDB_API_KEY);
+
+            if (apiKey == null)
+                throw new NullReferenceException(OMDB_API_KEY);
+
             services.AddSingleton<IOmdbService, OmdbService>();
             services.AddSingleton(typeof(IApiKeyProvider),
                 new ApiKeyProvider(apiKey));
             services.AddSingleton<IGetSearchResults, GetSearchResults>();
             services.AddSingleton<IGetFilmById, GetFilmById>();
+        }
+
+        public static void AddFilmScannerDb(this IServiceCollection services)
+        {
+            var connectionString = Environment.GetEnvironmentVariable(DB_CONNECTION_STRING);
+
+            if (connectionString == null)
+                throw new NullReferenceException(DB_CONNECTION_STRING);
+
+            services.AddDbContext<RepositoryContext>(options =>
+                options.UseNpgsql(connectionString));
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
         }
     }
 }
--- FilmScanner.API/Program.cs
+++ FilmScanner.API/Program.cs
@@ -1,8 +1,4 @@
 using FilmScanner.API.Extensions;
-using FilmScanner.Contracts;
-using FilmScanner.Entities;
-using FilmScanner.Repository;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -10,10 +6,8 @@ var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOmdbServices(builder.Configuration.GetValue<string>("OmdbApiKey"));
-builder.Services.AddDbContext<RepositoryContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+builder.Services.AddOmdbServices();
+builder.Services.AddFilmScannerDb();
 builder.Services.AddCors(a => a.AddPolicy(name: MyAllowSpecificOrigins, policy =>
 {
     policy.WithOrigins("http://localhost:4200")
@@ -22,13 +16,11 @@ builder.Services.AddCors(a => a.AddPolicy(name: MyAllowSpecificOrigins, policy =
 }));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
 
